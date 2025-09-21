7c4c4c784d5b18a236c8e649044901d24e1a6160
Agasthi16 <arunaagasthi@gmail.com>
2025-09-21 12:55:32 +0530

    Init customer html

--- Spice_Ceylon_Backend/src/main/java/lk/ijse/aad/spice_ceylon_backend/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package lk.ijse.aad.spice_ceylon_backend.repository;
-
-
-import lk.ijse.aad.spice_ceylon_backend.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
-
