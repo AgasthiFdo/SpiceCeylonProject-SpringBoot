b1f08bf18835e0cc492aaea64481be31220a6acd
Agasthi16 <arunaagasthi@gmail.com>
2025-09-21 12:26:24 +0530

    Init customer info

---
+++ Spice_Ceylon_Backend/src/main/java/lk/ijse/aad/spice_ceylon_backend/entity/Customer.java
@@ -0,0 +1,21 @@
+package lk.ijse.aad.spice_ceylon_backend.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "customers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String phone;
+    private String address;
+    private String payment;
+}
