d17f4b6f660bfca6de2ab4eaf6b4259d7ab6380d
Agasthi16 <arunaagasthi@gmail.com>
2025-09-23 21:54:55 +0530

    update Application

--- Spice_Ceylon_Backend/src/main/java/lk/ijse/aad/spice_ceylon_backend/SpiceCeylonBackendApplication.java
+++ Spice_Ceylon_Backend/src/main/java/lk/ijse/aad/spice_ceylon_backend/SpiceCeylonBackendApplication.java
@@ -1,7 +1,10 @@
 package lk.ijse.aad.spice_ceylon_backend;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class SpiceCeylonBackendApplication {
@@ -9,5 +12,10 @@ public class SpiceCeylonBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpiceCeylonBackendApplication.class, args);
     }
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 
 }
+
